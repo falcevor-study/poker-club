10a1bb7c977ad0458768742e2642fd87139af51a
falcevor <action@doctor.com>
2018-01-21 04:56:27 +0300

    I DID IT!!!11!!11! + Created template for game page. + Created created images of deck and table. + Implemented access to static resources. (css and image) (OH SHIT MAN, GOD DAMN) + Created configuration adapter for reference to resources. (YEH, I DID IT) ~ Configured security protocol for access to static resources. (MOTHERFUCKER)

--- src/main/java/kubsu/fctam/configuration/SecurityConfiguration.java
+++ src/main/java/kubsu/fctam/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/css/**")
+                .antMatchers("/image/**");
     }
 
     @Override
---
+++ src/main/java/kubsu/fctam/configuration/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package kubsu.fctam.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
+        registry.addResourceHandler("/image/**").addResourceLocations("/WEB-INF/image/");
+    }
+}
